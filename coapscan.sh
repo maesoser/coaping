9d9ed35f2781b97668b441a84e238da245324e00
maesoser <massesos@gmail.com>
2016-11-15 16:59:22 +0100

    addded tiny scan script

---
+++ coapscan.sh
@@ -0,0 +1,12 @@
+while :
+do
+while
+  set $(dd if=/dev/urandom bs=4 count=1 2>/dev/null | od -An -tu1)
+  [ $1 -lt 224 ] &&
+  [ $1 -ne 10 ] &&
+  { [ $1 -ne 192 ] || [ $2 -ne 168 ]; } &&
+  { [ $1 -ne 172 ] || [ $2 -lt 16 ] || [ $2 -gt 31 ]; }
+do :; done
+ip_address=$1.$2.$3.$4
+./coaping -n 1 $ip_address
+done
